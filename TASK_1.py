0f271b76c7faa22b63d592ae6f024f5a745152d2
aishvitha-7526 <aishvithaganapuram@gmail.com>
2026-05-17 08:53:06 +0530

    Add files via upload

---
+++ TASK_1.py
@@ -0,0 +1,30 @@
+# Caesar Cipher Program
+
+def encrypt(text, shift):
+    result = ""
+
+    for char in text:
+        if char.isalpha():
+            start = ord('A') if char.isupper() else ord('a')
+            encrypted_char = chr((ord(char) - start + shift) % 26 + start)
+            result += encrypted_char
+        else:
+            result += char
+
+    return result
+
+
+def decrypt(text, shift):
+    return encrypt(text, -shift)
+
+
+print("=== Caesar Cipher ===")
+
+message = input("Enter message: ")
+shift = int(input("Enter shift value: "))
+
+encrypted = encrypt(message, shift)
+print("\nEncrypted Message:", encrypted)
+
+decrypted = decrypt(encrypted, shift)
+print("Decrypted Message:", decrypted)
